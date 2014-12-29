d9f8362c8329ce95d5f0a936fcc7f884044424d4
Olivier Mehani <shtrom@ssji.net>
2014-12-30 00:09:21 +1100

    Allocate and deallocate bitmaps as needed
    Signed-off-by: Olivier Mehani <shtrom@ssji.net>

--- src/MinistryOfSillyWatches.c
+++ src/MinistryOfSillyWatches.c
@@ -2,31 +2,21 @@
 
 static Window *window;
 static BitmapLayer *bitmap_layer;
-static GBitmap *face_0_bitmap;
-static GBitmap *face_1_bitmap;
-static GBitmap *face_2_bitmap;
-static GBitmap *face_3_bitmap;
-static GBitmap *face_4_bitmap;
-static GBitmap *face_5_bitmap;
-static GBitmap *face_6_bitmap;
-static GBitmap *face_7_bitmap;
-static GBitmap *face_8_bitmap;
-static GBitmap *face_9_bitmap;
-static GBitmap *face_10_bitmap;
-static GBitmap *face_11_bitmap;
+static GBitmap *face_bitmap;
 
-#define SET_FACE(FACE_ID) do {						\
-  if (!face_ ## FACE_ID ## _bitmap) {					\
-    face_ ## FACE_ID ## _bitmap =					\
-      gbitmap_create_with_resource(RESOURCE_ID_FACE_ ## FACE_ID);	\
-    if (!face_ ## FACE_ID ## _bitmap) {					\
-      APP_LOG(APP_LOG_LEVEL_ERROR, "%d: face allocation error for %d",	\
-	  FACE_ID, RESOURCE_ID_FACE_ ## FACE_ID);			\
-    }									\
-  }									\
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "%d: showing face %p",			\
-      FACE_ID, face_ ## FACE_ID ## _bitmap);				\
-  bitmap_layer_set_bitmap(bitmap_layer, face_ ## FACE_ID ## _bitmap);	\
+#define SET_FACE(FACE_ID) do {							\
+  GBitmap *old_face_bitmap = face_bitmap;					\
+  face_bitmap = gbitmap_create_with_resource(RESOURCE_ID_FACE_ ## FACE_ID);	\
+  if (!face_bitmap) {								\
+    APP_LOG(APP_LOG_LEVEL_ERROR, "%d: face allocation error for %d",		\
+	FACE_ID, RESOURCE_ID_FACE_ ## FACE_ID);					\
+  }										\
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "%d: showing face %p",				\
+      FACE_ID, face_bitmap);							\
+  bitmap_layer_set_bitmap(bitmap_layer, face_bitmap);				\
+  if (old_face_bitmap) {							\
+    gbitmap_destroy(old_face_bitmap);						\
+  }										\
 } while(0)
 
 static void update_time(struct tm *tick_time) {
@@ -90,42 +80,6 @@ static void window_load(Window *window) {
 }
 
 static void window_unload(Window *window) {
-  if (face_0_bitmap) {
-    gbitmap_destroy(face_0_bitmap);
-  }
-  if (face_1_bitmap) {
-    gbitmap_destroy(face_1_bitmap);
-  }
-  if (face_2_bitmap) {
-    gbitmap_destroy(face_2_bitmap);
-  }
-  if (face_3_bitmap) {
-    gbitmap_destroy(face_3_bitmap);
-  }
-  if (face_4_bitmap) {
-    gbitmap_destroy(face_4_bitmap);
-  }
-  if (face_5_bitmap) {
-    gbitmap_destroy(face_5_bitmap);
-  }
-  if (face_6_bitmap) {
-    gbitmap_destroy(face_6_bitmap);
-  }
-  if (face_7_bitmap) {
-    gbitmap_destroy(face_7_bitmap);
-  }
-  if (face_8_bitmap) {
-    gbitmap_destroy(face_8_bitmap);
-  }
-  if (face_9_bitmap) {
-    gbitmap_destroy(face_9_bitmap);
-  }
-  if (face_10_bitmap) {
-    gbitmap_destroy(face_10_bitmap);
-  }
-  if (face_11_bitmap) {
-    gbitmap_destroy(face_11_bitmap);
-  }
   bitmap_layer_destroy(bitmap_layer);
 }
 
